80aa30329aa442d2c4f965237747b1512d19afa5
omer <omer@192.168.0.13>
2018-12-01 22:56:41 +0300

    matplotlib added

--- src/opencvlearn.py
+++ src/opencvlearn.py
@@ -4,15 +4,17 @@
 
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
 # Load an color image in grayscale
 img = cv2.imread('/Users/omer/NetBeansProjects/video/images/messi5.jpg',0)
 
-
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+plt.show()
 # cv2.imshow('image',img)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
 
-cv2.imwrite('/Users/omer/NetBeansProjects/video/images/messigray.png',img)
-
+# cv2.imwrite('/Users/omer/NetBeansProjects/video/images/messigray.png',img)
