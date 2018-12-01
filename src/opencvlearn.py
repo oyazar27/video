c3ef5b2eff99d8818b31c16d32163d0bd746d44e
omer <omer@192.168.0.13>
2018-12-01 20:36:29 +0300

    messi

--- src/opencvlearn.py
+++ src/opencvlearn.py
@@ -2,5 +2,17 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-if __name__ == "__main__":
-    print "Hello World3"
+import numpy as np
+import cv2
+
+# Load an color image in grayscale
+img = cv2.imread('/Users/omer/NetBeansProjects/video/images/messi5.jpg',0)
+
+
+# cv2.imshow('image',img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+cv2.imwrite('/Users/omer/NetBeansProjects/video/images/messigray.png',img)
+
